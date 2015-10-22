54985b9df59a5560dc8270645953d4e32f1e968a
Ashley Coleman <ashley@yieldify.com>
2015-10-22 18:03:38 +0100

    Initial commit

---
+++ new-npm-package.sh
@@ -0,0 +1,13 @@
+mkdir src/
+
+cat <<EOF >src/index.js
+require('coffee-script/register');
+require('babel/register');
+require('app');
+EOF
+
+cat <<EOF >src/app.coffee
+console.log 'My body is ready'
+EOF
+
+subl .
