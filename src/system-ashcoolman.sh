4e5e7bd7d6446cb70891b6070cadac5858e5a010
Ashley Coolman <writetofish@gmail.com>
2015-11-01 15:25:46 +0000

    update (system-ashcoolman): Added a few helpers

--- src/system-ashcoolman.sh
+++ src/system-ashcoolman.sh
@@ -2,3 +2,13 @@
 function gh(){
   cd ~/github/$1
 }
+
+function yy(){
+  cd ~/yieldify/$1
+}
+
+function cxm(){
+  cd ~/yieldify/cxm/$1
+}
+
+alias memcachestart="launchctl load /usr/local/opt/postgresql/homebrew.mxcl.postgresql.plist"
