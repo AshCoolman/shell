af9b6d1b995d46717f03333d5b706a288fa56eed
Ashley Coolman <writetofish@gmail.com>
2015-11-05 19:23:23 +0000

    update (new-npm-package): Updated with more scaffolding

--- src/javascript/new-npm-package.command.sh
+++ src/javascript/new-npm-package.command.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+# http://stackoverflow.com/questions/25329241/edit-package-json-from-commandline
+
 newnpm () {
     echo $(
-mkdir src
+mkdir src -p
 
 cat <<EOF >src/index.js
 require('coffee-script/register');
@@ -12,5 +14,22 @@ EOF
 cat <<EOF >src/app.coffee
 console.log 'My body is ready'
 EOF
-)
+
+mkdir test -p
+
+cat <<EOF >src/test.js
+require('coffee-script/register');
+require('babel/register');
+require('../src/app');
+EOF
+
+npm init --force
+
+json -I -f package.json -e 'this.scripts.test="node ./src/index.js"'
+
+git init
+
+git add -A
+
+git commit -m "Initial commit"
 }
