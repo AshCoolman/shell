df18c7dbd1b2418a337e8a7c237bc091a2f56ed2
Ashley Coolman <writetofish@gmail.com>
2015-11-05 19:23:47 +0000

    update (gitinit): Added more scaffolding

--- src/git.sh
+++ src/git.sh
@@ -4,3 +4,20 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.shuk 'stash -u -k' # stash only untracked files
 git config --global alias.last 'log -1 HEAD'
+
+gitinit () {
+
+    echo "# New project" >> README.md
+    git init
+    git add README.md
+    echo ".DS_Store
+npm-debug.log
+dist/
+doc/
+node_modules/
+bower_components/
+jspm_packages/
+*.sublime-*
+*.nogit.*
+**/*.nogit.*" >> .gitignore
+}
