baa8cc28642b53913a5bfd53686292dd40487b1d
Ashley Coolman <writetofish@gmail.com>
2015-11-11 23:21:22 +0000

    update (applications): Added VSCode, Abstract application

--- src/application.sh
+++ src/application.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
+
+# Applications
 alias subl="sublime"
+
+code () {
+    VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;
+}
+
+# Abstract applications
+alias codeeditor="sublime"
+
+# Edit bash
 alias bashed="sublime ~/.bash_profile"
