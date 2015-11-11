0934948c4f0217206aa949fa2c3bd6131fc9c917
Ashley Coolman <writetofish@gmail.com>
2015-11-11 23:23:12 +0000

    update (newnpm): Fixes and improvements

--- src/javascript/new-npm-package.command.sh
+++ src/javascript/new-npm-package.command.sh
@@ -3,7 +3,7 @@
 
 newnpm () {
     echo $(
-mkdir src -p
+mkdir -p src
 
 cat <<EOF >src/index.js
 require('coffee-script/register');
@@ -15,21 +15,40 @@ cat <<EOF >src/app.coffee
 console.log 'My body is ready'
 EOF
 
-mkdir test -p
+mkdir -p test
 
-cat <<EOF >src/test.js
+cat <<EOF >test/index.js
 require('coffee-script/register');
 require('babel/register');
 require('../src/app');
 EOF
 
+
+cat <<EOF >.gitignore
+node_modules/
+bower_components/
+EOF
+
+
+
+cat <<EOF >.babelrc
+{
+    "plugins": ["es2015"]
+}
+EOF
+
 npm init --force
 
+npm i babel-core coffee-script --save
+npm i babel-preset-es2015 --save-dev
+
+json -I -f package.json -e 'this.main="./src/index.js"'
+json -I -f package.json -e 'this.version="0.0.0"'
+json -I -f package.json -e 'this.scripts.test="node ./test/index.js"'
 json -I -f package.json -e 'this.scripts.test="node ./src/index.js"'
 
 git init
 
-git add -A
-
-git commit -m "Initial commit"
+codeeditor .
+)
 }
