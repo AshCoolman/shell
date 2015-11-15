f1bb87e6d85e1db08bb2bf6dd5e0305f0cfe99d8
Ashley Coolman <writetofish@gmail.com>
2015-11-15 22:18:50 +0000

    updated (run-once)

--- src/javascript/run-once.sh
+++ src/javascript/run-once.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 npm install -g json
 npm install -g nodemon
+npm install -g yeoman-generator
+npm install -g yo
+npm install -g npm
+npm install -g bower
+
+npm install -g coffee-script
+npm install -g webpack
+npm install -g gulp
+npm install -g gulp-coffee
+npm install -g node-inspector
+npm install -g protractor
+npm install -g karma
+npm install -g babel-cli
+npm install -g phantomjs
+
+npm install -g typescript
+npm install -g live-server
+npm install -g inquirer
+
+git config --global push.default simple
