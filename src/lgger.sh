a059750fbe703e3e3c0280ecfaa62d85e29c8fba
Ashley Coolman <writetofish@gmail.com>
2015-11-20 00:55:29 +0000

    update (lgger): more options

--- src/lgger.sh
+++ src/lgger.sh
@@ -1,12 +1,40 @@
 # ~/.bashrc
+# Log
 function lg() {
-  echo $(date +"%Y-%d-%m %T"): $USER$ $@ >> ~/my-log.txt
+  echo $(date +"%Y-%d-%m %T"): $USER$ $@ >> ~/lgger.txt
 }
 
+# Log bullet level 1
+function lgb() {
+  echo $(date +"%Y-%d-%m %T"): $USER$ * $@ >> ~/lgger.txt
+}
+
+# Log bullet level 2
+function lgbb() {
+  echo $(date +"%Y-%d-%m %T"): $USER$   * $@ >> ~/lgger.txt
+}
+
+# Log bullet level 3
+function lgbbb() {
+  echo $(date +"%Y-%d-%m %T"): $USER$     * $@ >> ~/lgger.txt
+}
+
+
+# Log list item (auto increment number)
 function lgl() {
-  echo "log list not implemented"
+  last=$(tail -n 1 ~/lgger.txt)
+  msg=$(cut -d "$" -f 2 <<< $last)
+  beforedot=$(cut -f1 -d"." <<< $msg)
+  re='^[0-9]+$'
+  if ! [[ $beforedot =~ $re ]] ; then
+    echo $(date +"%Y-%d-%m %T"): $USER$ 1. $@ >> ~/lgger.txt
+  else
+    num=`expr $beforedot + 1`
+    echo $(date +"%Y-%d-%m %T"): $USER$ $num. $@ >> ~/lgger.txt
+  fi
 }
 
-function lgout() {
+# output log
+function olg() {
   tail -n ${1-10} ~/my-log.txt
 }
