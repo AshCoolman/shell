26463cd1f7739d81d5278da5d020cf22c9c2fa20
Ashley Coolman <writetofish@gmail.com>
2015-12-18 23:32:50 +0000

    update: Edit and apply bash files

--- src/application.sh
+++ src/application.sh
@@ -10,6 +10,12 @@ code () {
 # Abstract applications
 alias codeeditor="sublime"
 
-# Edit bash
+# Edit bash files
 alias bashed="sublime ~/.bash_profile"
 alias bashit=". ~/.bash_profile"
+
+alias profed="sublime ~/.profile"
+alias profit=". ~/.profile"
+
+alias barced="sublime ~/.bashrc"
+alias barcit=". ~/.bashrc"
